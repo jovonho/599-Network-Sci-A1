5f3e84434175085978c3d3be2985ac6ebdf72588
Loic Ho-Von <loic.hovon@gmail.com>
2021-09-19 17:31:33 -0400

    NEW version of AB model ensures m edges are created for each node

--- ABmodel.py
+++ ABmodel.py
@@ -1,6 +1,5 @@
 
 import collections
-from os import link
 from scipy import sparse
 import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix, csc_matrix
@@ -194,18 +193,115 @@ def generate_AB_graph(n, m, save_to_file=False):
     return graph.tocsr()
 
 
+
+
+# Complexity: O(n^2) but much quicker than randomly picking candidates in practice
+def generate_AB_graph_ensure_m_edges(n, m, save_to_file=False):
+
+    # Initialize our data structures
+
+    # These list are in COO format, where an Matrix(row[i], col[i]) = 1 if there is a link 
+    # We want our graph to be undirected so we insert edges in both directions.
+    row = [0,1]
+    col = [1,0]
+
+    # This dictionary will keep track of the degrees of each nodes
+    nodes = {
+        0: 1,
+        1: 1
+    }
+
+    total_degree = 2
+
+    for i in range(2, n):
+
+        # Copy the current nodes and their degree
+        candidate_nodes = nodes.copy()
+        # Must get the keys te be able to remove values from it while iterating
+        candidate_node_keys = list(candidate_nodes.keys())
+        total_degree_at_t = total_degree
+        links_made = 0
+
+        log.debug(f"Adding new node {i}")
+        log.debug(f"\tDegrees at time t={i}: {candidate_nodes}")
+
+        while links_made < m:
+
+            # Here we iterate over the existing nodes always in the same order
+            for k, k_deg in candidate_nodes.items():   
+                
+                log.debug(f"\tTrying to link node {k}") 
+
+                # Compute the probability of linking node k to i
+                p = k_deg / total_degree_at_t
+                log.debug(f"\tProb of linking node {k} to {i}: {p}")
+
+                make_link = np.random.choice([0,1], p = [(1-p), p])
+
+                if make_link:
+                    links_made += 1
+
+                    log.debug(f"\t*** Nodes {k} and {i} linked!")
+
+
+                    # Add the new link to the row and col lists
+                    # We add the link in both directions (k, i) and (i, k) so the matrix will be symmetric
+                    row.extend([k, i])
+                    col.extend([i, k])
+
+                    # Increment the degrees of each node in the dictionary to use it when adding the next node
+                    if i in nodes:
+                        nodes[i] += 1
+                    else:
+                        nodes[i] = 1
+
+                    nodes[k] += 1
+
+                    # Total degree increases by 2, since we add symmetric links
+                    total_degree += 2
+
+                    if links_made == m:
+                        log.debug(f"\t******All {m} links made for node {i}!!")
+                        break
+
+            print(f"We've iterated through all the nodes. links made: {links_made} vs {m}")
+
+            # if links_made < m:
+            #     log.debug(f"\t*** No more candidate nodes. Links made for node {i}: {links_made}")
+
+    # Data will be all ones
+    nnz = len(row)    
+    data = np.ones(nnz)
+
+    if save_to_file:
+        with open(f"./data/AB_ensure_n{n}_m{m}.edgelist.txt", "w") as outfile:
+            for i in range(0, n):
+                outfile.write(f"{row[i]}\t{col[i]}\n")
+            outfile.write("\n")
+
+
+    # Generate the sparse matrix only at the end, from the coordinate lists
+    graph = coo_matrix((data, (row, col)),  shape=(n, n), dtype=np.int32)
+
+
+    return graph.tocsr()
+
+
+
+
+
 def main():
 
-    log.setLevel(logging.INFO)
+    # log.setLevel(logging.INFO)
 
-    # log.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
 
     n=50
     m=2
 
 
     t1 = time.time()
-    graph = generate_AB_graph(n=n, m=m, save_to_file=True)
+    graph = generate_AB_graph_ensure_m_edges(n=n, m=m, save_to_file=True)
     t2 = time.time()
     print(f"Time to generate AB model(n={n}, m={m}) with version 2: {t2-t1} s")
 
@@ -219,6 +315,10 @@ def main():
     A_minus_AT = graph - graph.T  
 
     print(f"\nIs A symmetric? (Is A - A.T all zeros?): {A_minus_AT.nnz == 0}")
+
+    degrees = [sum(line) for line in graph]
+
+    print(f"degrees: {degrees}")
 
     plot_degree_distrib(graph)
 
