8a22d8fc2a28b3ebae446a92745b03bc17a9d509
Loic Ho-Von <loic.hovon@gmail.com>
2021-09-20 20:58:34 -0400

    Cleaned up the code

--- A1.py
+++ A1.py
@@ -1,20 +1,11 @@
-from functools import total_ordering
-from heapq import nsmallest
-from math import isnan
-from os import altsep
 from typing import Counter
-from matplotlib import lines
-from networkx.algorithms.bipartite.basic import color
 import numpy as np
-from numpy.core.fromnumeric import mean, size, sort
 import scipy as sp
-from scipy import sparse, linalg, stats
-import pandas as pd
+from scipy import sparse, stats
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from numpy.polynomial import Polynomial
 import time
-from scipy.sparse import csgraph
 from scipy.sparse.csr import csr_matrix
 import seaborn as sns
 import math
@@ -22,46 +13,24 @@ import pathlib
 
 
 from BA_model import (
-    generate_BA_graph,
     generate_BA_graph_ensure_m_edges,
     generate_BA_graph_preferential_inattachment,
 )
 
+# Add a @profile annotation before any function to dump a memory usage analysis
 from memory_profiler import profile
-
-# Complexity: O(E)
-# We iterate through the edges once
-def remove_self_edges(edgelist):
-    orig_len = edgelist.shape[0]
-
-    self_edge_idx = []
-
-    for i in range(0, orig_len):
-        if edgelist[i][0] == edgelist[i][1]:
-            self_edge_idx.append(i)
-
-    print(f"Self edge indices: {self_edge_idx}")
-
-    edgelist = np.delete(edgelist, self_edge_idx, axis=0)
-
-    print(f"{orig_len - edgelist.shape[0]} self edges removed")
-    print(f"{edgelist.shape[0]} rows remain")
-
-    return edgelist
 
 
 def make_symmetric(edgelist):
 
-    print(f"\nNumber edges before removing (i, j) (j, i) pairs {len(edgelist)}")
-
-    # O(E) time, O(E) space
+    print(f"\nNumber edges before removing (i, j) (j, i) and (i, i) edges {len(edgelist)}")
 
     edgelist = [frozenset(edge) for edge in edgelist if edge[0] != edge[1]]
     edgelist = set(edgelist)
     edgelist = list(edgelist)
     edgelist = np.array([list(edge) for edge in edgelist])
 
-    print(f"Number edges after removing (i, j) (j, i) pairs {len(edgelist)}\n")
+    print(f"Number edges after removing (i, j) (j, i) and (i, i) edges {len(edgelist)}\n")
 
     return edgelist
 
@@ -89,29 +58,22 @@ def sanity_check(graph, details=False):
 
 def load_matrix(filename="./data/metabolic.edgelist.txt"):
     t1 = time.time()
+
     edgelist = np.loadtxt(filename, dtype=np.int32)
 
     edgelist = make_symmetric(edgelist)
 
-    # Create the graph in CSC form
+    # Create the graph in COO form, then convert to CSR
     rows = edgelist[:, 0]
     cols = edgelist[:, 1]
     data = np.ones(len(edgelist))
 
-    # print(list(rows))
-
     n = np.amax(edgelist) + 1
+
     graph = sparse.coo_matrix((data, (rows, cols)), shape=(n, n), dtype=np.int32)
     A = graph.tocsr()
 
     A = graph + graph.T
-
-    print(f"A nnz: {A.nnz}")
-    print(f"A shape: {A.shape}")
-    degrees = get_degrees(A)
-    total_degree = sum(degrees)
-    print(f"\nTotal degree: {total_degree}")
-    print(f"Average degree: {total_degree / A.shape[0]}")
 
     t2 = time.time()
     print(f"Time to load the graph: {t2-t1} s")
@@ -119,15 +81,14 @@ def load_matrix(filename="./data/metabolic.edgelist.txt"):
     return A
 
 
-def a_plot_degree_distrib(A, model_name, save_plot=False):
+def a_plot_degree_distrib(A, model_name, show_plot=True, save_plot=False):
 
     degrees = get_degrees(A)
 
     total_degree = sum(degrees)
-    print(f"A nnz: {A.nnz}")
     print(f"A shape: {A.shape}")
-    print(f"\nTotal degree: {total_degree}")
-    print(f"Average degree: {total_degree / A.shape[0]}")
+    print(f"Total degree: {total_degree}")
+    print(f"Average degree: {total_degree / A.shape[0]}\n")
 
     if save_plot:
         with open(f"./figs/{model_name}/model_stats.txt", "w") as outfile:
@@ -162,19 +123,14 @@ def a_plot_degree_distrib(A, model_name, save_plot=False):
 
     bins = np.logspace(start=0, stop=stop, base=10, num=num_bins, endpoint=True)
 
-    # Subtract 1 to get 0-based indices
     digitized = np.digitize(degrees, bins)
 
-    # print(f"digitized: {digitized}")
-
     bin_counts = Counter(digitized)
-    # print(f"Nn_count: {bin_counts}")
 
     Nn = []
 
     for i in range(0, num_bins):
         count = bin_counts[i]
-        # print(f"There are {count} nodes in bin {i}")
         Nn.append(count)
 
     while len(Nn) < (num_bins - 1):
@@ -209,7 +165,6 @@ def a_plot_degree_distrib(A, model_name, save_plot=False):
             pkn.remove(pkn_copy[i])
 
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(13, 7))
-    # fig, ax2 = plt.subplots(nrows=1, ncols=1, figsize=(8, 7))
 
     ax1.set_title("Raw Data")
     ax1.set_ylabel("P(k)")
@@ -234,7 +189,7 @@ def a_plot_degree_distrib(A, model_name, save_plot=False):
     ax2.scatter(kn, pkn)
 
     fit1 = Polynomial.fit(np.log10(k), np.log10(pk), deg=1)
-    print(f"Gamma (unbinned): {fit1.coef[0]}")
+    print(f"\nGamma (unbinned): {fit1.coef[0]}")
 
     yfit1 = lambda x: np.power(10, fit1(np.log10(x)))
     ax1.plot(k, yfit1(k), c="r", label=f"Exp: {fit1.coef[0]}")
@@ -250,11 +205,12 @@ def a_plot_degree_distrib(A, model_name, save_plot=False):
     fig.suptitle(f"{model_name}: Degree Distribution")
     fig.tight_layout()
 
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/1 - Degree Distrib.png", format="png")
-        plt.close()
-    else:
+    if show_plot:
         plt.show()
+
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/1 - Degree Distrib.png", format="png")
+        plt.close()
 
 
 def get_degrees(graph):
@@ -263,13 +219,6 @@ def get_degrees(graph):
 
 
 def get_clustering_coefs(graph, avg=False):
-
-    # TODO Remove this, just to check our results
-    import networkx as nx
-
-    G = nx.from_scipy_sparse_matrix(graph)
-    print(f"NX avg clustering of graph: {nx.average_clustering(G)}")
-
     t1 = time.time()
 
     A3 = graph ** 3
@@ -286,7 +235,7 @@ def get_clustering_coefs(graph, avg=False):
 
     clustering_coefs = np.array(clustering_coefs, dtype=np.float32)
 
-    print(f"My avg clustering: {sum(clustering_coefs) / N}")
+    print(f"\nAverage clustering coefficient: {sum(clustering_coefs) / N}")
 
     t2 = time.time()
 
@@ -298,10 +247,8 @@ def get_clustering_coefs(graph, avg=False):
     return clustering_coefs, deg
 
 
-def b_plot_clustering_coef_distrib(A, model_name, save_plot=False):
+def b_plot_clustering_coef_distrib(A, model_name, show_plot=True, save_plot=False):
     cc, _, avg_cc = get_clustering_coefs(A, avg=True)
-
-    print(f"\nAvg cc: {avg_cc}")
 
     fig, ax = plt.subplots(figsize=(13, 7))
 
@@ -327,14 +274,15 @@ def b_plot_clustering_coef_distrib(A, model_name, save_plot=False):
 
     fig.tight_layout()
 
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/2 - Clustering Coef.png", format="png")
-        plt.close()
-    else:
+    if show_plot:
         plt.show()
 
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/2 - Clustering Coef.png", format="png")
+        plt.close()
 
-def c_plot_shortest_paths(graph, model_name, save_plot=False):
+
+def c_plot_shortest_paths(graph, model_name, show_plot=True, save_plot=False):
     t1 = time.time()
 
     N = graph.shape[0]
@@ -347,12 +295,12 @@ def c_plot_shortest_paths(graph, model_name, save_plot=False):
     # As in the NI book Section 10.2
     avg_sp = shortest_paths.sum() / (N ** 2)
 
-    print(f"My Average path length: {avg_sp}")
+    print(f"\nAverage path length: {avg_sp}")
 
     shortest_paths = csr_matrix(shortest_paths)
 
     t2 = time.time()
-    print(f"Time to get shortest paths: {t2-t1} s")
+    print(f"Time to get paths: {t2-t1} s")
 
     fig, ax = plt.subplots(figsize=(9, 6))
 
@@ -373,11 +321,12 @@ def c_plot_shortest_paths(graph, model_name, save_plot=False):
 
     fig.tight_layout()
 
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/3 - Shortest Paths.png", format="png")
-        plt.close()
-    else:
+    if show_plot:
         plt.show()
+
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/3 - Shortest Paths.png", format="png")
+        plt.close()
 
 
 def d_get_connected_compo(graph, model_name, save_plot=False):
@@ -395,19 +344,18 @@ def d_get_connected_compo(graph, model_name, save_plot=False):
 
     nodes_in_GCC = GCC_len / N
 
-    t2 = time.time()
-    print(f"Time to get comnnected components: {t2-t1} s")
-
     if save_plot:
         with open(f"./figs/{model_name}/4 - Connected Components.txt", "w") as outfile:
             outfile.write(f"Number of Connected Components:\t\t{con_comp}\n")
             outfile.write(f"Proportion of nodes in GCC:\t\t{nodes_in_GCC}\n")
 
-    print(f"Number of Connected Components:\t\t{con_comp}\n")
-    print(f"Proportion of nodes in GCC:\t\t{nodes_in_GCC}\n")
+    print(f"\nNumber of Connected Components:\t\t{con_comp}")
+    print(f"Proportion of nodes in GCC:\t\t{nodes_in_GCC}")
+    t2 = time.time()
+    print(f"Time to get comnnected components: {t2-t1} s")
 
 
-def e_get_eigenval_distrib(graph, model_name, k=100, save_plot=False):
+def e_get_eigenval_distrib(graph, model_name, k=100, show_plot=True, save_plot=False):
     t1 = time.time()
 
     L = sparse.csgraph.laplacian(graph)
@@ -415,7 +363,9 @@ def e_get_eigenval_distrib(graph, model_name, k=100, save_plot=False):
     eigenvals_L = sparse.linalg.eigsh(L.asfptype(), k=k, return_eigenvectors=False, which="SM")
 
     t2 = time.time()
-    print(f"Time to get {k} Eigenvals: {t2-t1} s")
+    print(f"\nTime to get {k} smallest Eigenvals: {t2-t1} s")
+
+    t1 = time.time()
 
     # Hack to get the number of zero values, we were getting extremely small but non-zero floats otherwise
     num_zeros = sparse.csgraph.connected_components(
@@ -426,9 +376,12 @@ def e_get_eigenval_distrib(graph, model_name, k=100, save_plot=False):
 
     if num_zeros < len(eigenvals_L):
         spectral_gap = eigenvals_L[len(eigenvals_L) - 1 - num_zeros]
-        print(f"Spectral gap: {spectral_gap}")
+        print(f"\nSpectral gap: {spectral_gap}")
     else:
-        print("Spectral gap: 0")
+        print("\nSpectral gap: 0")
+
+    t2 = time.time()
+    print(f"Time to get Spectral gap: {t2-t1} s")
 
     fig, ax = plt.subplots(figsize=(13, 7))
 
@@ -450,14 +403,15 @@ def e_get_eigenval_distrib(graph, model_name, k=100, save_plot=False):
     ax.legend()
     fig.tight_layout()
 
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/5 - Eigenvalues.png", format="png")
-        plt.close()
-    else:
+    if show_plot:
         plt.show()
 
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/5 - Eigenvalues.png", format="png")
+        plt.close()
 
-def f_get_degree_correl(graph, model_name, save_plot=False):
+
+def f_get_degree_correl(graph, model_name, show_plot=True, save_plot=False):
     t1 = time.time()
     degs = get_degrees(graph)
     n = degs.shape[0]
@@ -468,8 +422,6 @@ def f_get_degree_correl(graph, model_name, save_plot=False):
     # O(n2)
     # Efficient row slicing with CSR
     for i in range(n):
-        if i % 2000 == 0:
-            print(f"on row {i}")
         row = graph.getrow(i).toarray()[0]
 
         for j in range(n):
@@ -477,13 +429,12 @@ def f_get_degree_correl(graph, model_name, save_plot=False):
                 X.append(degs[i])
                 Y.append(degs[j])
 
-    t2 = time.time()
-    print(f"Time to calculate degree correlations: {t2-t1} s")
-
     # Doesn't seem like it should work but cross-referenced with NX and it was similar
     _, _, r_value, _, _ = stats.linregress(X, Y)
 
-    print(f"R value: {r_value}")
+    print(f"\nR value: {r_value}")
+    t2 = time.time()
+    print(f"Time to calculate degree correlations: {t2-t1} s")
 
     fig, ax = plt.subplots(figsize=(9, 6))
 
@@ -509,19 +460,17 @@ def f_get_degree_correl(graph, model_name, save_plot=False):
     ax.legend()
     fig.tight_layout()
 
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/6 - Degree Correlation.png", format="png")
-        plt.close()
-    else:
+    if show_plot:
         plt.show()
 
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/6 - Degree Correlation.png", format="png")
+        plt.close()
 
-def g_plot_clustering_degree_rel(A, model_name, save_plot=False):
+
+def g_plot_clustering_degree_rel(A, model_name, show_plot=True, save_plot=False):
 
     cc, d = get_clustering_coefs(A)
-
-    print(f"Size of CC: {len(cc)}")
-    print(f"Size of degrees: {len(d)}")
 
     fig, ax = plt.subplots(figsize=(13, 7))
 
@@ -543,81 +492,60 @@ def g_plot_clustering_degree_rel(A, model_name, save_plot=False):
     )
 
     fig.tight_layout()
-    if save_plot:
-        plt.savefig(f"./figs/{model_name}/7 - CC vs Degree.png", format="png")
-        plt.close()
-    else:
+
+    if show_plot:
         plt.show()
+
+    if save_plot:
+        fig.savefig(f"./figs/{model_name}/7 - CC vs Degree.png", format="png")
+        plt.close()
 
 
 def main():
     t1 = time.time()
+    print("\n")
 
-    print("\n\n")
+    """
+        Load and analyze the edgelist networks from Barabasi
+    """
 
     # G = load_matrix("./data/metabolic.edgelist.txt")
-    # # # G = load_matrix("./data/collaboration.edgelist.txt")
 
     # model_name = "Metabolic"
-    # a_plot_degree_distrib(G, model_name)
-
-    # G_BA_n1039_m5 = generate_BA_graph_ensure_m_edges(5400, 1)
-
-    # # model_name = "BA model n=4941 m=1 v2"
-    # model_name = "Protein BA model n=5400 m=1"
 
     # pathlib.Path(f"./figs/{model_name}").mkdir(parents=True, exist_ok=True)
 
-    # a_plot_degree_distrib(G_BA_n1039_m5, model_name)
+    # a_plot_degree_distrib(G, model_name, show_plot=True, save_plot=False)
+    # b_plot_clustering_coef_distrib(G, model_name, show_plot=True, save_plot=False)
+    # c_plot_shortest_paths(G, model_name, show_plot=True, save_plot=False)
+    # d_get_connected_compo(G, model_name, save_plot=False)
+    # e_get_eigenval_distrib(G, model_name, k=100, show_plot=True, save_plot=False)
+    # f_get_degree_correl(G, model_name, show_plot=True, save_plot=False)
+    # g_plot_clustering_degree_rel(G, model_name, show_plot=True, save_plot=False)
 
-    # b_plot_clustering_coef_distrib(G_BA_n1039_m5, model_name)
+    """
+        Generate and analyze a BA Model graph
+    """
 
-    # c_plot_shortest_paths(G_BA_n1039_m5, model_name)
+    n = 1500
+    m = 5
 
-    # d_get_connected_compo(G_BA_n1039_m5, model_name)
+    G = generate_BA_graph_ensure_m_edges(n=n, m=m)
 
-    # f_get_degree_correl(G_BA_n1039_m5, model_name)
-
-    # g_plot_clustering_degree_rel(G_BA_n1039_m5, model_name)
-
-    # e_get_eigenval_distrib(G_BA_n1039_m5, model_name, k=100)
-
-    # G_BA_n1039_m5 = generate_BA_graph_preferential_inattachment(2018, 2)
-
-    G_BA_n1039_m5 = generate_BA_graph_ensure_m_edges(2018, 2)
-
-    # model_name = "BA model n=4941 m=1 v2"
-    model_name = "Protein Imitation n=2018 m=2"
+    model_name = f"BA Model n={n} m={m}"
 
     pathlib.Path(f"./figs/{model_name}").mkdir(parents=True, exist_ok=True)
 
-    # a_plot_degree_distrib(G_BA_n1039_m5, model_name)
-
-    b_plot_clustering_coef_distrib(G_BA_n1039_m5, model_name, save_plot=True)
-
-    # c_plot_shortest_paths(G_BA_n1039_m5, model_name)
-
-    # d_get_connected_compo(G_BA_n1039_m5, model_name)
-
-    # f_get_degree_correl(G_BA_n1039_m5, model_name)
-
-    # g_plot_clustering_degree_rel(G_BA_n1039_m5, model_name)
-
-    # e_get_eigenval_distrib(G_BA_n1039_m5, model_name, k=100)
-
-    exit()
-    #
-    G_phonecalls = load_matrix("./data/phonecalls.edgelist.txt")
-    model_name = "Phonecalls"
-    e_get_eigenval_distrib(G_phonecalls, model_name, k=10)
+    a_plot_degree_distrib(G, model_name, show_plot=True, save_plot=False)
+    b_plot_clustering_coef_distrib(G, model_name, show_plot=True, save_plot=False)
+    c_plot_shortest_paths(G, model_name, show_plot=True, save_plot=False)
+    d_get_connected_compo(G, model_name, save_plot=False)
+    e_get_eigenval_distrib(G, model_name, k=100, show_plot=True, save_plot=False)
+    f_get_degree_correl(G, model_name, show_plot=True, save_plot=False)
+    g_plot_clustering_degree_rel(G, model_name, show_plot=True, save_plot=False)
 
     t2 = time.time()
-    print(f"Total running time: {t2-t1} s")
-
-    t2 = time.time()
-    print(f"Total running time: {t2-t1} s")
-
-    print("\n\n\n")
+    print(f"\nTotal running time: {t2-t1} s\n\n")
 
 
 if __name__ == "__main__":
--- BA_model.py
+++ BA_model.py
@@ -1,192 +1,24 @@
-import collections
-from scipy import sparse
 import numpy as np
-from scipy.sparse import coo_matrix, csr_matrix, csc_matrix
+from scipy.sparse import coo_matrix
 import time
 import logging
 from logging import handlers
-import matplotlib.pyplot as plt
+import sys
+
 
 log = logging.getLogger(__name__)
-
-import sys
 
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 
-fh = handlers.RotatingFileHandler(filename="./log/ABmodel.log", encoding="utf-8")
+fh = handlers.RotatingFileHandler(filename="./log/BA_model.log", encoding="utf-8")
 fh.setLevel(logging.DEBUG)
 
 log.addHandler(ch)
 log.addHandler(fh)
 
 
-def compare_degree_distrib(A1, A2):
-
-    degrees1 = np.array([sum(line) for line in A1.A])
-    total_degree1 = sum(degrees1)
-
-    print(f"Degrees: {degrees1}, len(degrees): {len(degrees1)}")
-    print(f"Total degree: {total_degree1}")
-
-    counts1 = collections.Counter(degrees1)
-
-    X1, Y1 = list(counts1.keys()), list(counts1.values())
-
-    degrees2 = np.array([sum(line) for line in A2.A])
-    total_degree2 = sum(degrees2)
-
-    print(f"Degrees: {degrees2}, len(degrees): {len(degrees2)}")
-    print(f"Total degree: {total_degree2}")
-
-    counts2 = collections.Counter(degrees2)
-
-    X2, Y2 = list(counts2.keys()), list(counts2.values())
-
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(13, 7))
-
-    ax1.set_title("Method 1: random candidate choice")
-    ax1.set_ylabel("P(k)")
-    ax1.set_xlabel("k")
-    ax1.set_xscale("log", base=10)
-    ax1.set_yscale("log", base=10)
-    ax1.tick_params(which="both", direction="in")
-
-    ax1.scatter(X1, Y1)
-
-    ax2.set_title("Method 2: iterating over existing nodes")
-    ax2.set_ylabel("P(k)")
-    ax2.set_xlabel("k")
-    ax2.set_xscale("log", base=10)
-    ax2.set_yscale("log", base=10)
-    ax2.tick_params(which="both", direction="in")
-
-    ax2.scatter(X2, Y2, c="r")
-
-    fig.tight_layout()
-    plt.show()
-
-
-# No binning, just raw data
-def plot_degree_distrib(A):
-
-    # degrees1 = np.array([sum(line) for line in A.A])
-
-    degrees1 = []
-    for i in range(A.shape[0]):
-        row = A.getrow(i)
-        row = row.toarray()[0]
-        print(f"Node {i} has degree: {sum(row)}")
-        degrees1.append(sum(row))
-
-    total_degree1 = sum(degrees1)
-
-    print(f"Degrees: {degrees1}, len(degrees): {len(degrees1)}")
-    print(f"Total degree: {total_degree1}")
-
-    counts1 = collections.Counter(degrees1)
-
-    X1, Y1 = list(counts1.keys()), list(counts1.values())
-
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(13, 7))
-
-    ax1.set_title("Method 1: random candidate choice")
-    ax1.set_ylabel("P(k)")
-    ax1.set_xlabel("k")
-    ax1.set_xscale("log", base=10)
-    ax1.set_yscale("log", base=10)
-    ax1.tick_params(which="both", direction="in")
-
-    ax1.scatter(X1, Y1)
-
-    fig.tight_layout()
-    plt.show()
-
-
-# Complexity: O(n^2) but much quicker than randomly picking candidates in practice
-def generate_BA_graph(n, m):
-
-    # Initialize our data structures
-
-    # These list are in COO format, where an Matrix(row[i], col[i]) = 1 if there is a link
-    # We want our graph to be undirected so we insert edges in both directions.
-    row = [0, 1]
-    col = [1, 0]
-
-    # This dictionary will keep track of the degrees of each nodes
-    nodes = {0: 1, 1: 1}
-
-    total_degree = 2
-
-    for i in range(2, n):
-
-        # Copy the current nodes and their degree
-        candidate_nodes = nodes.copy()
-        # Must get the keys te be able to remove values from it while iterating
-        candidate_node_keys = list(candidate_nodes.keys())
-        total_degree_at_t = total_degree
-        links_made = 0
-
-        log.debug(f"Adding new node {i}")
-        log.debug(f"\tDegrees at time t={i}: {candidate_nodes}")
-
-        # Here we iterate over the existing nodes always in the same order
-        for k, k_deg in candidate_nodes.items():
-
-            log.debug(f"\tTrying to link node {k}")
-
-            # Compute the probability of linking node k to i
-            p = k_deg / total_degree_at_t
-            log.debug(f"\tProb of linking node {k} to {i}: {p}")
-
-            make_link = np.random.choice([0, 1], p=[(1 - p), p])
-
-            if make_link:
-                links_made += 1
-
-                log.debug(f"\t*** Nodes {k} and {i} linked!")
-
-                # Add the new link to the row and col lists
-                # We add the link in both directions (k, i) and (i, k) so the matrix will be symmetric
-                row.extend([k, i])
-                col.extend([i, k])
-
-                # Increment the degrees of each node in the dictionary to use it when adding the next node
-                if i in nodes:
-                    nodes[i] += 1
-                else:
-                    nodes[i] = 1
-
-                nodes[k] += 1
-
-                # Total degree increases by 2, since we add symmetric links
-                total_degree += 2
-
-                if links_made == m:
-                    log.debug(f"\t******All {m} links made for node {i}!!")
-                    break
-
-        if links_made < m:
-            log.debug(f"\t*** No more candidate nodes. Links made for node {i}: {links_made}")
-
-    # Data will be all ones
-    nnz = len(row)
-    data = np.ones(nnz)
-
-    # TODO BROKEN
-    if False:
-        with open(f"./data/AB_n{n}_m{m}.edgelist.txt", "w") as outfile:
-            for i in range(0, n):
-                outfile.write(f"{row[i]}\t{col[i]}\n")
-            outfile.write("\n")
-
-    # Generate the sparse matrix only at the end, from the coordinate lists
-    graph = coo_matrix((data, (row, col)), shape=(n, n), dtype=np.int32)
-
-    return graph.tocsr()
-
-
-# Complexity: O(n^2) but much quicker than randomly picking candidates in practice
+# Complexity: O(n^2)
 def generate_BA_graph_ensure_m_edges(n, m):
     t1 = time.time()
 
@@ -213,8 +45,6 @@ def generate_BA_graph_ensure_m_edges(n, m):
 
         # Copy the current nodes and their degree
         candidate_nodes = nodes.copy()
-        # Must get the keys te be able to remove values from it while iterating
-        candidate_node_keys = list(candidate_nodes.keys())
         total_degree_at_t = total_degree
         links_made = 0
 
@@ -267,9 +97,6 @@ def generate_BA_graph_ensure_m_edges(n, m):
                         break
 
             log.debug(f"We've iterated through all the nodes. links made: {links_made} vs {m2}")
-
-            # if links_made < m:
-            #     log.debug(f"\t*** No more candidate nodes. Links made for node {i}: {links_made}")
 
     # Data will be all ones
     nnz = len(row)
@@ -359,22 +186,9 @@ def generate_BA_graph_preferential_inattachment(n, m):
 
             log.debug(f"We've iterated through all the nodes. links made: {links_made} vs {m}")
 
-            # if links_made < m:
-            #     log.debug(f"\t*** No more candidate nodes. Links made for node {i}: {links_made}")
-
     # Data will be all ones
     nnz = len(row)
     data = np.ones(nnz)
-
-    row_col = [(row[i], col[i]) for i in range(len(row))]
-    row_col = sorted(row_col)
-
-    # TODO: This is broken. When we read the matrix from the edgelist its all fucked up
-    if False:
-        with open(f"./data/AB_ensure_n{n}_m{m}.edgelist.txt", "w") as outfile:
-            for i in range(0, n):
-                print(f"Writing {row_col[i][0]}\t{row_col[i][1]}\n")
-                outfile.write(f"{row_col[i][0]}\t{row_col[i][1]}\n")
 
     # Generate the sparse matrix only at the end, from the coordinate lists
     graph = coo_matrix((data, (row, col)), shape=(n, n), dtype=np.int32)
@@ -386,20 +200,18 @@ def generate_BA_graph_preferential_inattachment(n, m):
 
 
 def main():
+    t1 = time.time()
 
-    # log.setLevel(logging.INFO)
-
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
+    # log.setLevel(logging.DEBUG)
 
     n = 1039
     m = 5
 
-    t1 = time.time()
-    graph = generate_BA_graph_ensure_m_edges(4000, 40)
+    graph = generate_BA_graph_ensure_m_edges(n, m)
+
     t2 = time.time()
     print(f"Time to generate AB model(n={n}, m={m}) with version 2: {t2-t1} s")
-
-    # compare_degree_distrib(graph1, graph2)
 
     print(f"Shape of graph: {graph.shape}")
     print(f"Graph nnz: {graph.nnz}")
@@ -411,10 +223,7 @@ def main():
     print(f"\nIs A symmetric? (Is A - A.T all zeros?): {A_minus_AT.nnz == 0}")
 
     degrees = [sum(line) for line in graph]
-
     print(f"degrees: {degrees}")
-
-    plot_degree_distrib(graph)
 
 
 if __name__ == "__main__":
